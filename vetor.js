feb45021fa7faa7b29eb4c9a9d31e3c3d0732889
Pedro Queiroga <psq@cin.ufpe.br>
2017-11-25 16:44:17 -0300

    objeto vetor

---
+++ vetor.js
@@ -0,0 +1,88 @@
+function Vetor(x, y, z) {
+    if (typeof x !== 'number' ||
+	typeof y !== 'number' ||
+	typeof z !== 'number') {
+	throw 'Não é um vetor';
+    }
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.norma = Math.sqrt(this.produtoInterno(this));
+}
+
+Vetor.prototype.igual = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	return false;
+    }
+    return this.x == v2.x && this.y == v2.y && this.z == v2.z;
+};
+
+Vetor.prototype.mais = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }
+    var v = new Vetor(this.x + v2.x,
+		      this.y + v2.y,
+		      this.z + v2.z);
+    return v;
+};
+
+Vetor.prototype.menos = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }
+    var v = new Vetor(this.x - v2.x,
+		      this.y - v2.y,
+		      this.z - v2.z);
+    return v;
+};
+
+Vetor.prototype.produtoEscalar = function(escalar) {
+    var v = new Vetor(this.x * escalar,
+		      this.y * escalar,
+		      this.z * escalar);
+    return v;
+};
+
+Vetor.prototype.produtoComponentes = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }
+    var v = new Vetor(this.x * v2.x,
+		      this.y * v2.y,
+		      this.z * v2.z);
+    return v;
+};
+
+Vetor.prototype.produtoInterno = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }
+    var pi = (this.x * v2.x) + (this.y * v2.y) + (this.z * v2.z);
+    return pi;
+};
+
+Vetor.prototype.produtoVetorial = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }    
+    var v = new Vetor(this.y * v2.z - this.z * v2.y,
+		      this.z * v2.x - this.x * v2.z,
+		      this.x * v2.y - this.y * v2.x);
+    return v;
+};
+
+Vetor.prototype.projecao = function(v2) {
+    if (!(v2 instanceof Vetor)) {
+	throw 'v2 não é um vetor';
+    }
+    var proj = v2.produtoEscalar(this.produtoInterno(v2)/v2.produtoInterno(v2));
+    return proj;
+};
+
+Vetor.prototype.normalizado = function() {
+    var v = new Vetor(this.x/this.norma,
+		      this.y/this.norma,
+		      this.z/this.norma);
+    return v;
+};
